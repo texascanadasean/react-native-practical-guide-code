1d0d4edafdc33fc5a9b5e89dcc28cdd07e628336
Sean Kenney <sean_kenney@hotmail.com>
2022-10-15 11:07:24 -0500

    added authtoken to api call

--- code/01-starting-code/screens/WelcomeScreen.js
+++ code/01-starting-code/screens/WelcomeScreen.js
@@ -1,10 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableNativeFeedbackComponent,
+  View,
+} from 'react-native';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://mobiletest-58ae8-default-rtdb.firebaseio.com/message.json?auth=' +
+          token,
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -16,11 +41,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 32
+    padding: 32,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 8
-  }
+    marginBottom: 8,
+  },
 });
