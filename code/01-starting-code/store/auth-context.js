3ff6509b48c554f9618e77b669bd8b3453c2cc8e
Sean Kenney <sean_kenney@hotmail.com>
2022-10-19 08:21:31 -0500

    using local state to store the auth token

--- code/01-starting-code/store/auth-context.js
+++ code/01-starting-code/store/auth-context.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({
   token: '',
@@ -10,8 +11,21 @@ export const AuthContext = createContext({
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState();
 
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token');
+
+      if (storedToken) {
+        setAuthToken(storedToken);
+      }
+    }
+
+    fetchToken();
+  }, []);
+
   function authenticate(token) {
     setAuthToken(token);
+    AsyncStorage.setItem('token', token);
   }
 
   function logout() {
